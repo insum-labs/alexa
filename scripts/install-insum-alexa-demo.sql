98e9098d8aef06a8d60868da289a2bd7d73c04f4
Martin D'Souza <martin@clarifit.com>
2018-03-09 06:35:25 -0700

    install / uninstall scripts

---
+++ scripts/install-insum-alexa-demo.sql
@@ -0,0 +1,17 @@
+-- This file can be run in sqlcl or SQL Dev
+
+-- Ords
+@https://raw.githubusercontent.com/insum-labs/alexa/master/scripts/create_askme_rest_service.sql
+
+-- Types
+@https://raw.githubusercontent.com/insum-labs/alexa/master/types/t_alexa_card.sql
+@https://raw.githubusercontent.com/insum-labs/alexa/master/types/t_alexa_output_speech.sql
+
+-- Packages
+@https://raw.githubusercontent.com/insum-labs/alexa/master/packages/alexa.pks
+@https://raw.githubusercontent.com/insum-labs/alexa/master/packages/alexa.pkb
+
+-- Demo app
+@https://raw.githubusercontent.com/insum-labs/alexa/master/packages/askme.pks
+@https://raw.githubusercontent.com/insum-labs/alexa/master/packages/askme.pkb
+
---
+++ scripts/uninstall-insum-alexa-demo.sql
@@ -0,0 +1,15 @@
+-- This file can be run in sqlcl or SQL Dev
+
+-- Ords
+@https://raw.githubusercontent.com/insum-labs/alexa/master/scripts/drop_all_rest_services.sql
+
+-- Types
+drop type t_alexa_card;
+drop type t_alexa_output_speech;
+
+-- Packages
+drop package alexa;
+
+-- Demo app
+drop package askme;
+
